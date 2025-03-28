870b118ee5827464f24a0bcd05f58b8f0765b26b
Promise-Digitals <promiseedeh025@gmail.com>
2025-03-28 06:51:20 +0100

    Update companyController.js

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -137,6 +137,39 @@ export const postJob = async (req, res) => {
 }
 
 
+// Get a single job data by ID
+export const getJobById = async (req, res) => {
+    
+    try {
+        
+        const {id} = req.params
+        
+        const job = await Job.findById(id).populate({
+            path: 'companyId',
+            select: '-password'
+        })
+
+        if (!job) {
+            res.json({
+                success: false,
+                message: "Job not Found"
+            })
+        }
+
+        res.json({
+            success: true,
+            job
+        })
+
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+
 // Get company job applicants
 export const getCompanyJobApplicants = async (req, res) => {
     
