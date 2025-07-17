661edc79b9b5d1d0aff7de2483702fefec19cef0
Promise-Digitals <promiseedeh025@gmail.com>
2025-07-17 22:16:11 +0100

    some changes

--- server/server.js
+++ server/server.js
@@ -28,8 +28,8 @@ app.use(cors({
 }))
 app.use(session({
     secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true,
+    resave: false,
+    saveUninitialized: false,
 }))
 
 app.use(passport.initialize())
